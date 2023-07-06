47b1ba0429918809bb0d1e66271c1ba05bbbff27
XaydarovMarufjon <marufjankhaydarov@gmail.com>
2023-07-06 16:01:41 +0500

    middle ware products

--- models/User.js
+++ models/User.js
@@ -5,6 +5,10 @@ const UserSchema = new Schema({
     phone : {type : String , required : true }, 
     email : {type : String , required : true , unique: true }, 
     password : {type : String , required : true }, 
+}
+, 
+{
+ timestamps: true ,
 })
 
 const User = model('User' , UserSchema)
